ef9ded0f6024e4dc737c0aaeb7e0d58fc0313c0d
Ivan Pedrazas <ipedrazas@gmail.com>
2014-11-02 22:20:51 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ docker pull ubuntu
 docker pull tpires/neo4j
 docker pull dockerfile/redis
 docker pull dockerfile/mongodb
+docker pull postgres
 docker pull ipedrazas/elasticsearch
 docker pull poklet/cassandra
 docker pull python:2
