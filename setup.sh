4c2bf8452b279d080eceee976a592988d2c7e9bc
Ivan Pedrazas <ipedrazas@gmail.com>
2014-11-06 11:03:33 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
 
+# Install latest docker
+sudo apt-key -y adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-get install lxc-docker
+
+# Docker without sudo
+
+sudo groupadd docker
+sudo gpasswd -a ${USER} docker
+sudo service docker restart
+
+
+# pull some images
 docker pull ubuntu
 docker pull tpires/neo4j
 docker pull dockerfile/redis
